8c48a5de089259effb5ae450b71a8f89865e8555
Sarah Aguasvivas <31457827+sarahaguasvivas@users.noreply.github.com>
2018-04-23 19:59:07 -0600

    Add files via upload

--- tireCollection/prepared/tireLearning.py
+++ tireCollection/prepared/tireLearning.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python2.7
 import numpy as np
 #import keras
+from scipy import signal 
+import matplotlib.pyplot as plt
 
 data= np.array([])
-filename= "Carpet.txt"
-with open(filename) as f:
-    line= f.readlines()
-    u=np.fromstring(line, dtype=float, sep= ',')
-    data= np.append(data,u)
+filename= "FakeGrass.txt"
 
-BUFFER_SIZE= 500
-print data
-for i in xrange(0, f.shape[0], BUFFER_SIZE):
-    Window= f[:i, :]
-    print Window
+data= np.loadtxt(filename, delimiter= ',', usecols=range(4))
+
+BUFFER_SIZE= 1000
+WINDOW_LEFT_SIZE=200
+WINDOW_RIGHT_SIZE= 300 
+ 
+for i in xrange(BUFFER_SIZE, data.shape[0], BUFFER_SIZE):
+    Window= data[i-BUFFER_SIZE:i, :]
+    envelope= signal.hilbert(Window[:, 0])
+    envelope= envelope.real
+    plt.plot(envelope)
+    b,a= signal.butter(8, 100, 'low', analog=True)
+    envelope= signal.filtfilt(b,a, envelope, padlen=50)
+    plt.plot(envelope)
+    plt.plot(Window[:, 0])
+    plt.show()
 
