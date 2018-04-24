8e0ffe979c68f2c8dfb961a575e533e3bba5f433
Sarah Aguasvivas <31457827+sarahaguasvivas@users.noreply.github.com>
2018-04-24 08:20:09 -0600

    finding peaks

--- tireCollection/prepared/tireLearning.py
+++ tireCollection/prepared/tireLearning.py
@@ -9,18 +9,28 @@ filename= "FakeGrass.txt"
 
 data= np.loadtxt(filename, delimiter= ',', usecols=range(4))
 
-BUFFER_SIZE= 1000
+BUFFER_SIZE= 300 
 WINDOW_LEFT_SIZE=200
 WINDOW_RIGHT_SIZE= 300 
- 
+count=0
+peaks= np.array([])
+xpeaks=np.array([])
 for i in xrange(BUFFER_SIZE, data.shape[0], BUFFER_SIZE):
     Window= data[i-BUFFER_SIZE:i, :]
     envelope= signal.hilbert(Window[:, 0])
-    envelope= envelope.real
-    plt.plot(envelope)
-    b,a= signal.butter(8, 100, 'low', analog=True)
-    envelope= signal.filtfilt(b,a, envelope, padlen=50)
-    plt.plot(envelope)
-    plt.plot(Window[:, 0])
-    plt.show()
+    peak= signal.argrelextrema(envelope, np.greater)
+    window= Window[peak, 0]
+    
+    if np.max(window) > 0.5:
+        maxim= np.max(window)
+        print maxim
+        peaks1= np.where(window==maxim) 
+        print peaks1
+        xpeaks= np.append(xpeaks, peaks1)
+        peaks= np.append(peaks, maxim)
+        count+=1
+print count  
+plt.plot(data[:, 0])
+plt.plot(xpeaks,peaks, 'ro')
+plt.show()
 
