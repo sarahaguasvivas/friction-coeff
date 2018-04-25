f736c7c0d3a5b1e4a7e448da662c6d529e4447c2
sarahaguasvivas <saag5228@colorado.edu>
2018-04-25 09:55:32 -0600

    new code

--- tireCollection/prepared/tireLearning.py
+++ tireCollection/prepared/tireLearning.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 
 data= np.array([])
 filename= "FakeGrass.txt"
-
+dataFile= "data1FakeGrass.txt"
 data= np.loadtxt(filename, delimiter= ',', usecols=range(4))
 
-BUFFER_SIZE= 800
+BUFFER_SIZE= 500 #500 worked
 WINDOW_LEFT_SIZE=200
 WINDOW_RIGHT_SIZE= 300
 count=0
@@ -23,15 +23,16 @@ for i in xrange(BUFFER_SIZE, data.shape[0], BUFFER_SIZE):
     envelope= signal.hilbert(filtered)
     envelope= np.abs(envelope)
     peak= signal.argrelextrema(envelope, np.greater)
-    plt.plot(envelope)
-    plt.plot(Window[:, 0])
-    plt.show()
     window= Window[peak, 0]
     maxim= np.max(window)
-    if maxim > 0.4:
-        peaks1= np.where(window==maxim)
+    print window
+    if maxim > 0.3:
+        peaks1= np.where(envelope==maxim) 
+        print peaks1
         xpeaks= np.append(xpeaks, peaks1)
         peaks=  np.append(peaks, maxim)
-        count+=1
-print count
-
+        j= i-BUFFER_SIZE+peaks1
+        np.savetxt(dataFile, data[j-WINDOW_LEFT_SIZE: j+WINDOW_LEFT_SIZE, :], delimiter= ',')
+        #print data[j-WINDOW_LEFT_SIZE: j+WINDOW_LEFT_SIZE, :]
+        plt.plot(data[j-WINDOW_LEFT_SIZE: j+WINDOW_LEFT_SIZE, :])
+        plt.show()
