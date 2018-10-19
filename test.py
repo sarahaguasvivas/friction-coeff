d82b82e9a74717153128fc8302f648317a1d97a1
sarahaguasvivas <saag5228@colorado.edu>
2018-10-19 09:51:36 -0600

    working CNN

--- test.py
+++ test.py
@@ -13,6 +13,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
 """Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
 
 from __future__ import absolute_import
@@ -53,7 +54,7 @@ def cnn_model_fn(features, labels, mode):
   # Output Tensor Shape: [batch_size, 14, 14, 64]
   conv2 = tf.layers.conv2d(
       inputs=pool1,
-      filters=64,
+      filters=32,
       kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.relu)
@@ -72,14 +73,14 @@ def cnn_model_fn(features, labels, mode):
   # Input Tensor Shape: [batch_size, 7 * 7 * 64]
   # Output Tensor Shape: [batch_size, 1024]
   dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-
   # Add dropout operation; 0.6 probability that element will be kept
   dropout = tf.layers.dropout(
       inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
-
+  
   # Input Tensor Shape: [batch_size, 1024]
   # Output Tensor Shape: [batch_size, 10]
   logits = tf.layers.dense(inputs=dropout, units=2)
+
   predictions = {
       # Generate predictions (for PREDICT and EVAL mode)
       "classes": tf.argmax(input=logits, axis=1),
@@ -87,27 +88,29 @@ def cnn_model_fn(features, labels, mode):
       # `logging_hook`.
       "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
   }
-  
+
   if mode == tf.estimator.ModeKeys.PREDICT:
     return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
   # Calculate Loss (for both TRAIN and EVAL modes)
-  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits))
-
+  logits= tf.transpose(logits) 
+  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+  
   # Configure the Training Op (for TRAIN mode)
   if mode == tf.estimator.ModeKeys.TRAIN:
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-3)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-3) 
     train_op = optimizer.minimize(
         loss=loss,
         global_step=tf.train.get_global_step())
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
  
   # Add evaluation metrics (for EVAL mode)
+  
   eval_metric_ops = {
       "accuracy": tf.metrics.accuracy(
           labels=labels, predictions=predictions["classes"])}
   return tf.estimator.EstimatorSpec(
       mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
+  
 def main(unused_argv):
   # Load training and eval data
   train_data = np.load('terrains_train_features.npy') 
@@ -130,7 +133,7 @@ def main(unused_argv):
   tensors_to_log = {"probabilities": "softmax_tensor"}
   logging_hook = tf.train.LoggingTensorHook(
       tensors=tensors_to_log, every_n_iter=50)
-
+ 
   # Train the model
   train_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={"x": train_data},
@@ -138,6 +141,7 @@ def main(unused_argv):
       batch_size=2,
       num_epochs=None,
       shuffle=True)
+  
   terrains_classifier.train(
       input_fn=train_input_fn,
       steps=20000,
@@ -150,7 +154,7 @@ def main(unused_argv):
       num_epochs=1,
       shuffle=False)
   eval_results = terrains_classifier.evaluate(input_fn=eval_input_fn)
-#  print(eval_results)
+  print(eval_results)
 
 
 if __name__ == "__main__":
